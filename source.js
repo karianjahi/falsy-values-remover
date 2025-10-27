bc121e863541e3cc119804aba4e18e4f1bae964a
karianjahi <kitgaedesigaidi8@gmail.com>
2025-10-27 21:26:29 +0100

    Complete the function that removes falsy values

--- source.js
+++ source.js
@@ -1,3 +1,21 @@
 /* A function that removes all falsy values from an array */
 const bouncer = (array) => {
+    let cleanedArray = [];
+  for (let item of array) {
+    // console.log(`${item} ------ ${typeof(item)}. Result is actually a ${typeof(typeof(item))}`);
+    if (typeof item == "number" && isNaN(item)) {
+      // getting rid of NaN Values
+      ("do nothing");
+    } else {
+      if (
+        item !== false &&
+        item !== null &&
+        item !== 0 &&
+        item !== "" &&
+        item !== undefined
+      )
+        cleanedArray.push(item);
+    }
+  }
+  return cleanedArray;
 }
