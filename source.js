91198037284378a6299609e555d1d649fa47b98f
karianjahi <kitgaedesigaidi8@gmail.com>
2025-10-27 21:25:44 +0100

    Add empty function

--- source.js
+++ source.js
@@ -1,6 +1,3 @@
-// A function that converts a string to title case where each first letter in a word is capitalized and the rest is lower case
-const titleCase = (aString) => {
-
-
-    
+/* A function that removes all falsy values from an array */
+const bouncer = (array) => {
 }
